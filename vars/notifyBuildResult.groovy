b690a4a68d64c14f3f0f64cc49cc711eb32548a3
Richard Cai <rcai@xogrp.com>
2020-03-30 23:47:35 +0800

    Replace currentBuild.result with currentBuild.currentResult

--- vars/notifyBuildResult.groovy
+++ vars/notifyBuildResult.groovy
@@ -7,11 +7,11 @@ def call(String channel) {
     }
 
     def elapsedTime = currentBuild.durationString.replace(' and counting', '');
-    def slackColor = currentBuild.result == 'SUCCESS' ? 'good' : 'danger';
+    def slackColor = currentBuild.currentResult == 'SUCCESS' ? 'good' : 'danger';
 
     slackSend(
         color: slackColor,
-        message: "$REPO_NAME (${env.BRANCH_NAME}) - ${currentBuild.result} after $elapsedTime (<${env.BUILD_URL}|${env.BUILD_DISPLAY_NAME}>)",
+        message: "$REPO_NAME (${env.BRANCH_NAME}) - ${currentBuild.currentResult} after $elapsedTime (<${env.BUILD_URL}|${env.BUILD_DISPLAY_NAME}>)",
         channel: channel
     )
 }
