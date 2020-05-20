cdf579f4032fe776b896bca5ae687a5ebd620961
Richard Cai <rcai@xogrp.com>
2020-05-20 12:01:49 +0800

    Make necessary changes to support different apps: - Change getFriendlyFileName to support to different apps - Generate commit web url based on different repo.

--- vars/getFriendlyFileName.groovy
+++ vars/getFriendlyFileName.groovy
@@ -2,7 +2,9 @@
 
 /*
  * Generate friendly file name based on current branch name
- * @param {String} buildType: It indicate which environment we would like to build app for
+ * @param {String} buildType: It indicates which environment we would like to build app for
+ * @param {String} appName: It's used to distiguish different app.
+ * @param {Boolean} includedSquad: To decide whether to include squad name or not
  * Remarks:
  *   As current logic, we only want to handle the branch name with prefix "features/",
  *   but I would like to extend it to something like "angan/190326-refactor_timeline",
@@ -17,15 +19,19 @@
  *     - BRANCH_NAME
  *     - BUILD_NUMBER
  */
-def call(String buildType) {
+def call(String buildType, String appName = 'Planner', Boolean includedSquad = true) {
     def branchName = env.BRANCH_NAME
     // Analyse to get squad name
-    def squadName = getSquadName(branchName)
+    def squadName = ''
+
+    if (includedSquad) {
+        squadName = getSquadName(branchName)
+    }
     // Analyse to get feature description,
     // feature description should include squad name
     def sections = branchName.split('/')
     // TODO: Since this description will be used in file name, so we should consider to filter some special words.
     def featureDescription = sections[sections.size() - 1].replaceFirst(/^(local|guest|core|registry)\-/, '')
 
-    return "Planner-${buildType}-${squadName + (squadName ? '-' : '')}${featureDescription + (featureDescription ? '-' : '')}${env.BUILD_NUMBER}.apk"
+    return "${appName}-${buildType}-${squadName + (squadName ? '-' : '')}${featureDescription + (featureDescription ? '-' : '')}${env.BUILD_NUMBER}.apk"
 }
--- vars/notifyBuildAPKs.groovy
+++ vars/notifyBuildAPKs.groovy
@@ -15,11 +15,10 @@ def changelogsForSlack(Integer maxLogCount = 9) {
     def JIRA_TICKET_PATTERN = /([A-Z]{3,10}\-\d{4,})/
     def PIVOTAL_BASE_URL = 'https://www.pivotaltracker.com/story/show/'
     def JIRA_BASE_URL = 'https://theknotww.atlassian.net/browse/'
-    def GIT_COMMIT_BASE_URL = "https://github.com/tkww/Planner-Android/commit/"
 
     for (changeSet in currentBuild.changeSets) {
         for (entry in changeSet.items) {
-            def finalMessage = "${entry.msg} by ${entry.author.displayName} (<${GIT_COMMIT_BASE_URL + entry.commitId}|${entry.commitId.substring(0, 7)}>)"
+            def finalMessage = "${entry.msg} by ${entry.author.displayName} (<${changeSet.browser.url}commit/${entry.commitId}|${entry.commitId.substring(0, 7)}>)"
 
             if (finalMessage.find(PIVOTAL_TICKET_PATTERN)) {
                  finalMessage = finalMessage.replaceAll(PIVOTAL_TICKET_PATTERN, "<${PIVOTAL_BASE_URL}\$2|\$1>")
