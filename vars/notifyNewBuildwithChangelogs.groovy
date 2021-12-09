0facc45f724ba7b355f12af8a1185a5df897b4ab
Richard Cai <rcai@xogrp.com>
2021-12-09 20:42:59 +0800

    Add a new method to send slack notification with changelogs, but without APP links.

---
+++ vars/notifyNewBuildwithChangelogs.groovy
@@ -0,0 +1,56 @@
+#!/usr/bin/env groovy
+
+/*
+ * Send build message with new changelogs
+ *
+ * @param {String} channel, It indicates which Slack channel(s) you would like to post
+ * @param {String} buildType, It indicates current build type, such as Debug, Stg, Release.
+ *
+ * Dependencies:
+ *     - BRANCH_NAME
+ *     - BUILD_NUMBER
+ */
+def call(channel, buildType = "Debug") {
+    if (!channel) {
+        error('Missing channel to send message to')
+
+        return
+    }
+
+    slackSend(
+        channel: channel,
+        "attachments": [
+            [
+                "mrkdwn_in": ["text"],
+                "color": "#36a64f",
+                "pretext": "App Build Succeeded!",
+                // "author_name": "Richard Cai",
+                "title": "Changelogs",
+                // "title_link": "https://www.theknot.com",
+                "text": "${generateChangelogsForSlack()}",
+                "fields": [
+                    [
+                        "title": "App Name",
+                        "value": "${env.REPO_NAME}",
+                        "short": true
+                    ],
+                    [
+                        "title": "Branch",
+                        "value": "${env.BRANCH_NAME}",
+                        "short": true
+                    ],
+                    [
+                        "title": "Build Number",
+                        "value": "${env.BUILD_NUMBER}",
+                        "short": true
+                    ],
+                    [
+                        "title": "Build Type",
+                        "value": "${buildType}",
+                        "short": false
+                    ]
+                ]
+            ]
+        ]
+    )
+}
